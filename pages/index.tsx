e8c67792adc272f2d3eed0e3afef55a7c276298a
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2022-05-15 20:14:23 +0200

    Revert "Testing TailwindCSS." - It works. Both the `className` and `@apply` methods.
    This reverts commit 20b8515030add27b97b19b56628b5d91c60cd5e0.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <span className="text-zinc-600">Welcome to </span>
-          <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
